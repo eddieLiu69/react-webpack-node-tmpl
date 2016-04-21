66b710aed1c526619a48bef2c42f275ca1bf8e5e
Eddie_liu <eddie_liu@htc.com>
2016-04-21 14:42:37 +0800

    refactoring for typescript

--- app/server.tsx
+++ app/server.tsx
@@ -25,11 +25,11 @@ axios.defaults.baseURL = `http://${clientConfig.host}:${clientConfig.port}`;
  * @param initial state of the store, so that the client can be hydrated with the same state as the server
  * @param head - optional arguments to be placed into the head
  */
-function renderFullPage(renderedContent, initialState, head = {
+const renderFullPage = (renderedContent, initialState, head = {
   title: 'React Webpack Node',
   meta: '<meta name="viewport" content="width=device-width, initial-scale=1" />',
   link: '<link rel="stylesheet" href="/assets/styles/main.css"/>'
-}) {
+}) => {
   return `
     <!doctype html>
     <html lang="">
@@ -56,69 +56,70 @@ function renderFullPage(renderedContent, initialState, head = {
  * We grab the state passed in from the server and the req object from Express/Koa
  * and pass it into the Router.run function.
  */
-export default function render(req, res) {
-  const history = createMemoryHistory();
-  // const authenticated = req.isAuthenticated();
-  const store = configureStore({
-    // user: {
-    //   // authenticated: authenticated,
-    //   isWaiting: false,
-    //   message: '',
-    //   isLogin: true
-    // }
-  }, history);
+export default () => {
+  return (req, res) => {
+    const history = createMemoryHistory();
+    // const authenticated = req.isAuthenticated();
+    const store = configureStore({
+      // user: {
+      //   // authenticated: authenticated,
+      //   isWaiting: false,
+      //   message: '',
+      //   isLogin: true
+      // }
+    }, history);
 
-  const routes = createRoutes(store);
+    const routes = createRoutes(store);
 
-  /*
-   * From the react-router docs:
-   *
-   * This function is to be used for server-side rendering. It matches a set of routes to
-   * a location, without rendering, and calls a callback(error, redirectLocation, renderProps)
-   * when it's done.
-   *
-   * The function will create a `history` for you, passing additional `options` to create it.
-   * These options can include `basename` to control the base name for URLs, as well as the pair
-   * of `parseQueryString` and `stringifyQuery` to control query string parsing and serializing.
-   * You can also pass in an already instantiated `history` object, which can be constructured
-   * however you like.
-   *
-   * The three arguments to the callback function you pass to `match` are:
-   * - error: A javascript Error object if an error occured, `undefined` otherwise.
-   * - redirectLocation: A `Location` object if the route is a redirect, `undefined` otherwise
-   * - renderProps: The props you should pass to the routing context if the route matched, `undefined`
-   *                otherwise.
-   * If all three parameters are `undefined`, this means that there was no route found matching the
-   * given location.
-   */
-  match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message);
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    } else if (renderProps) {
-      const InitialView = (
-        <Provider store={store}>
-          <RouterContext {...renderProps as any} />
-        </Provider>
-      );
-
-      //This method waits for all render component promises to resolve before returning to browser
-      fetchComponentDataBeforeRender(store.dispatch, renderProps.components, renderProps.params)
-        .then(html => {
-          const componentHTML = renderToString(InitialView);
-          const initialState = store.getState();
-          res.status(200).end(renderFullPage(componentHTML, initialState, {
-            title: headconfig.title as any,
-            meta: headconfig.meta as any,
-            link: headconfig.link as any
-          }));
-        })
-        .catch(err => {
-          res.end(renderFullPage("", {}));
-        });
-    } else {
-      res.status(404).send('Not Found');
-    }
-  });
+    /*
+    * From the react-router docs:
+    *
+    * This function is to be used for server-side rendering. It matches a set of routes to
+    * a location, without rendering, and calls a callback(error, redirectLocation, renderProps)
+    * when it's done.
+    *
+    * The function will create a `history` for you, passing additional `options` to create it.
+    * These options can include `basename` to control the base name for URLs, as well as the pair
+    * of `parseQueryString` and `stringifyQuery` to control query string parsing and serializing.
+    * You can also pass in an already instantiated `history` object, which can be constructured
+    * however you like.
+    *
+    * The three arguments to the callback function you pass to `match` are:
+    * - error: A javascript Error object if an error occured, `undefined` otherwise.
+    * - redirectLocation: A `Location` object if the route is a redirect, `undefined` otherwise
+    * - renderProps: The props you should pass to the routing context if the route matched, `undefined`
+    *                otherwise.
+    * If all three parameters are `undefined`, this means that there was no route found matching the
+    * given location.
+    */
+    match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+      if (error) {
+        res.status(500).send(error.message);
+      } else if (redirectLocation) {
+        res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+      } else if (renderProps) {
+        const InitialView = (
+          <Provider store={store}>
+            <RouterContext {...renderProps as any} />
+          </Provider>
+        );
+        //This method waits for all render component promises to resolve before returning to browser
+        fetchComponentDataBeforeRender(store.dispatch, renderProps.components, renderProps.params)
+          .then(html => {
+            const componentHTML = renderToString(InitialView);
+            const initialState = store.getState();
+            res.status(200).end(renderFullPage(componentHTML, initialState, {
+              title: headconfig.title as any,
+              meta: headconfig.meta as any,
+              link: headconfig.link as any
+            }));
+          })
+          .catch(err => {
+            res.end(renderFullPage("", {}));
+          });
+      } else {
+        res.status(404).send('Not Found');
+      }
+    });
+  }
 }
--- server/config/routes.ts
+++ server/config/routes.ts
@@ -21,8 +21,6 @@ const routes = (app) => {
   // This is where the magic happens. We take the locals data we have already
   // fetched and seed our stores with data.
   // App is a function that requires store data and url to initialize and return the React-rendered html string
-  app.get("*", (req, res, next) => {
-    App.default(req, res);
-  });
+  app.get("*", App.default());
 };
 export default routes;
